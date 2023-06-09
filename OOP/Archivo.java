1a66a2f5eb77d0abd7bd5e95a69d13d3179c4c54
César Arana <95942498+carana08@users.noreply.github.com>
2023-06-08 22:07:16 -0500

    Delete OOP directory

--- OOP/Archivo.java
+++
@@ -1,6 +0,0 @@
-package OOP;
-
-public interface Archivo {
-	public void seleccionarArchivo();
-	public void verArchivo();
-}
--- OOP/Usuario.java
+++
@@ -1,7 +0,0 @@
-package OOP;
-public class Usuario {
-	public void visualizarArchivo(Archivo archivo) {
-		archivo.seleccionarArchivo();
-		archivo.verArchivo();
-	}
-}
